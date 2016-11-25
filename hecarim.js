89ed29e80ce7f513c5bb574c18b338224c5190f3
Patrick B. Buehl <pbb91@yahoo.com>
2016-11-25 00:07:14 -0600

    Make hecarim cursor exist

--- hecarim.js
+++ hecarim.js
@@ -3,8 +3,11 @@
  */
 $(document).ready(function(){
 
+    $("head").append("<link rel='stylesheet' type='text/css' href='hecarim.css' />");
+
     function hecarimTest() {
         console.log("hecarim passive");
     }
-    $( "body" ).on( "click", hecarimTest );
+    $("body").on( "click", hecarimTest );
+    
 });
