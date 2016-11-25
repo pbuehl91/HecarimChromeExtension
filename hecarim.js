c9611761137f1d98c4134c78cc4d11cf080df10c
Patrick B. Buehl <pbb91@yahoo.com>
2016-11-24 23:29:14 -0600

    Add basic hecarim functionality

---
+++ hecarim.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Patrick on 11/24/2016.
+ */
+$(document).ready(function(){
+
+    function hecarimTest() {
+        console.log("hecarim passive");
+    }
+    $( "body" ).on( "click", hecarimTest );
+});
